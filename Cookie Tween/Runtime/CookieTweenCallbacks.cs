6841fe94747d605810504394148e7f16a8d77b9e
Dev-Squishy <55626689+Dev-Squishy@users.noreply.github.com>
2021-04-05 23:11:01 -0500

    Add files via upload

---
+++ Cookie Tween/Runtime/CookieTweenCallbacks.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace CookieTweenie
+{
+    public class CookieTweenCallbacks : MonoBehaviour
+    {
+        public List<IUIEffect> effects;
+        
+    }
+}
---
+++ Cookie Tween/Runtime/UIFade.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace CookieTweenie
+{
+    public class UIFade : IUIEffect
+    {
+        private Image UiImage { get; }
+        private float Alpha { get; }
+        private float LerpSpeed { get; }
+
+        public UIFade(Image image, float alpha, float speed)
+        {
+            UiImage = image;
+            Alpha = alpha;
+            LerpSpeed = speed;
+        }
+
+        public IEnumerator Execute()
+        {
+            var time = 0f;
+            var currentAlpha = UiImage.color.a;
+
+            while (UiImage.color.a != Alpha)
+            {
+                time += Time.deltaTime * LerpSpeed;
+                var alphaToSet = Mathf.Lerp(currentAlpha, Alpha, time);
+
+                Color temp = UiImage.color;
+                temp.a = alphaToSet;
+                UiImage.color = temp;
+                yield return null;
+            }
+        }
+    }
+}
